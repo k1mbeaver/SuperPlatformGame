546c3799de9a3fcf510a2532da95f3457c618926
k1mbeaver <83329956+k1mbeaver@users.noreply.github.com>
2023-04-28 23:44:50 +0900

    Add files via upload
    AI 몬스터의 애님 인스턴스 추가

---
+++ Source/Capstone_Test/MyAIAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MyAIAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAPSTONE_TEST_API UMyAIAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
