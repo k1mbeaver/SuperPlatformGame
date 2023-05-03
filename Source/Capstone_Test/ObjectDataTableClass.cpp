f7f0c4bf55420daebdf8be33663ab0e0b0adcbdf
k1mbeaver <83329956+k1mbeaver@users.noreply.github.com>
2023-05-03 21:29:14 +0900

    Add files via upload
    오브젝트에 대한 정보를 게임 인스턴스에서 가져오게 하기위함

---
+++ Source/Capstone_Test/ObjectDataTableClass.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ObjectDataTableClass.h"
+
+// Sets default values
+AObjectDataTableClass::AObjectDataTableClass()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AObjectDataTableClass::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AObjectDataTableClass::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
