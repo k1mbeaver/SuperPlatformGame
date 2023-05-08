26dd8184e288d2f216817302c07d1df3c9381acf
k1mbeaver <83329956+k1mbeaver@users.noreply.github.com>
2023-05-08 21:57:37 +0900

    Add files via upload
    메인 UI 클래스 생성

---
+++ Source/Capstone_Test/PlayerMainUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PlayerMainUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAPSTONE_TEST_API UPlayerMainUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
