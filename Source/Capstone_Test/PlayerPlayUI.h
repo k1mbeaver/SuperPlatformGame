1d0c5db75cefd0eda63b98d54dfd9db555b90280
k1mbeaver <83329956+k1mbeaver@users.noreply.github.com>
2023-05-09 23:43:07 +0900

    Add files via upload
    메뉴와 게임 플레이 UI 클래스 생성

---
+++ Source/Capstone_Test/PlayerPlayUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PlayerPlayUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAPSTONE_TEST_API UPlayerPlayUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
