359aee12f67030c585046ba94f3a7ba8d2495a85
k1mbeaver <83329956+k1mbeaver@users.noreply.github.com>
2023-05-10 23:21:38 +0900

    Add files via upload
    AIController 클래스를 생성해서 블랙보드, 비헤이비어 트리를 사용할 수 있게 하자

--- Source/Capstone_Test/MyAICharacter.h
+++ Source/Capstone_Test/MyAICharacter.h
@@ -15,15 +15,128 @@ public:
 	// Sets default values for this character's properties
 	AMyAICharacter();
 
+	//UPROPERTY(VisibleInstanceOnly, Category = Animation)
+		//class UAIAnimInstance* AIAnim;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = Attack, Meta = (AllowPrivateAccess = true))
+		float AttackPower;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = Attack, Meta = (AllowPrivateAccess = true))
+		float AttackRange;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = Attack, Meta = (AllowPrivateAccess = true))
+		float AttackRadius;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = Attack, Meta = (AllowPrivateAccess = true))
+		float SkillPower;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = Attack, Meta = (AllowPrivateAccess = true))
+		float SkillRange;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = Attack, Meta = (AllowPrivateAccess = true))
+		float SkillRadius;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = Speed, Meta = (AllowPrivateAccess = true))
+		float MonsterSpeed;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = Attack, Meta = (AllowPrivateAccess = true))
+		bool bCanAttack;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = HP, Meta = (AllowPrivateAccess = true))
+		float fAIHp;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = HP, Meta = (AllowPrivateAccess = true))
+		float fMaxHp;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Meta = (AllowPrivateAccess = true))
+		int nMonsterType;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly)
+		bool bAlive;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly)
+		bool bUIPrint;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+		FString strMonsterType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+		FString strMonsterItem;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+		USoundBase* DeathSound;
+
+	//UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Attack, Meta = (AllowPrivateAccess = true))
+		//UAnimMontage* AttackMontage;
+
+	//UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Attack, Meta = (AllowPrivateAccess = true))
+		//UAnimMontage* IntroMontage;
+
+	//UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Attack, Meta = (AllowPrivateAccess = true))
+		//UAnimMontage* SkillMontage;
+
+	UPROPERTY(VisibleInstanceOnly, Category = Animation)
+		class UMyAIAnimInstance* MonsterAnim;
+
+	//UPROPERTY()
+		//class UParticleSystem* AttackParticle;
+
+	//UPROPERTY()
+		//class UParticleSystem* IntroParticle;
+
+	//UPROPERTY()
+		//class UParticleSystem* SkillParticle;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		USceneComponent* ParticleMuzzleLocation;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		USceneComponent* IntroParticleMuzzleLocation;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		USceneComponent* ProjectileMuzzle;
+
+	// damage particle
+	UPROPERTY()
+		class UParticleSystem* DamageParticle;
+
+	UPROPERTY()
+		class UGameplayStatics* GameStatic;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	//virtual void PostInitializeComponents() override;
+
+	//float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamasgeCauser) override;
+
+	//void Attack();
+	//void Skill();
+	//void AttackEnd();
+	//void SkillEnd();
+	//void AttackCheck();
+	//void SkillCheck();
+	//void AttackCheck();
+	//void OnAttackMontageEnded(UAnimMontage* Montage, bool bInterrupted);
+	//void AttackByPlayer(float DamageAmount);
+	//void MoveMonster();
+	//void FindPlayer();
+	//void StopMonster();
+	//void StopAIController();
+	//void InitializeAI(FString MonsterType);
+	//void AttackParticleStart(FVector StartParticle);
+	//void IntroParticleStart();
+	//void SkillParticleStart();
+	//void SkillParticleEnd();
+	//void DropItem();
+	//void MonsterDestory();
 
 };
