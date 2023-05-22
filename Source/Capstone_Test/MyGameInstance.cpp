42bdcc2a1a953c466dfac5605b57564c7cf535d2
k1mbeaver <83329956+k1mbeaver@users.noreply.github.com>
2023-05-22 21:53:45 +0900

    Add files via upload
    게임 인스턴스 클래스에 함수를 추가

--- Source/Capstone_Test/MyGameInstance.cpp
+++ Source/Capstone_Test/MyGameInstance.cpp
@@ -2,4 +2,23 @@
 
 
 #include "MyGameInstance.h"
+#include "AIDataTableClass.h"
+#include "CharacterDataTableClass.h"
+#include "ObjectDataTableClass.h"
 
+/*
+int UMyGameInstance::GetInventoryItemCount(int nSlot)
+{
+	FString strSlot = FString::FromInt(nSlot);
+	FInventoryDataTable* InventoryData = FInventoryTable->FindRow<FInventoryDataTable>(*strSlot, TEXT(""));
+	int ItemCount = InventoryData->ItemCount;
+	return ItemCount;
+}
+
+void UMyGameInstance::SetInventoryItemCount(int nSlot, int nCount)
+{
+	FString strSlot = FString::FromInt(nSlot);
+	FInventoryDataTable* InventoryData = FInventoryTable->FindRow<FInventoryDataTable>(*strSlot, TEXT(""));
+	InventoryData->ItemCount = nCount;
+}
+*/
--- Source/Capstone_Test/MyGameInstance.h
+++ Source/Capstone_Test/MyGameInstance.h
@@ -6,6 +6,11 @@
 #include "Engine/GameInstance.h"
 #include "MyGameInstance.generated.h"
 
+struct FAIDataTable;
+struct FObjectDataTable;
+struct FCharacterDataTable;
+class UDataTable;
+
 /**
  * 
  */
@@ -14,4 +19,24 @@ class CAPSTONE_TEST_API UMyGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 	
+public:
+	//UMyGameInstance();
+
+	// 플레이어 데이터 값 얻어오기
+	//USkeletalMesh* GetPlayerSkeletalMesh(FString PlayerType);
+	//float GetPlayerHP();
+	//float GetPlayerStamina();
+	//int GetPlayerStage();
+	//float GetPlayerMP();
+
+
+private:
+	UPROPERTY()
+		UDataTable* FAITable;
+
+	UPROPERTY()
+		UDataTable* FObjectTable;
+
+	UPROPERTY()
+		UDataTable* FCharacterTable;
 };
