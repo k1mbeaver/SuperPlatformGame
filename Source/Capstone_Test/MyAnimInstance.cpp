8a3ffaaa77c3a89328f782ecb93b507d9e7aee68
k1mbeaver <83329956+k1mbeaver@users.noreply.github.com>
2023-05-24 21:51:57 +0900

    Add files via upload
    캐릭터의 애니메이션을 좀 더 추가하기위해 애님 인스턴스를 새로 만들어서 진행

--- Source/Capstone_Test/MyAnimInstance.cpp
+++ Source/Capstone_Test/MyAnimInstance.cpp
@@ -2,4 +2,45 @@
 
 
 #include "MyAnimInstance.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/PawnMovementComponent.h"
 
+UMyAnimInstance::UMyAnimInstance()
+{
+	CurrentPawnSpeed = 0.0f;
+	IsInAir = false;
+	IsDead = false;
+
+	// 몽타주는 게임 인스턴스에서 직접 가져오는걸로?
+}
+
+void UMyAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	auto Pawn = TryGetPawnOwner(); // 폰에 접근해서 폰의 속력 값을 얻어온다.
+	if (!::IsValid(Pawn)) return;
+
+	if (!IsDead)
+	{
+		CurrentPawnSpeed = Pawn->GetVelocity().Size();
+		auto Character = Cast<ACharacter>(Pawn);
+		if (Character)
+		{
+			IsInAir = Character->GetMovementComponent()->IsFalling();
+		}
+	}
+}
+
+void UMyAnimInstance::PlayAttackMontage(UAnimMontage* GetAttackMontage)
+{
+	if (IsAttacking == true)
+	{
+		Montage_Play(GetAttackMontage, 1.0f);
+	}
+}
+
+void UMyAnimInstance::SetDeadAnim()
+{
+	IsDead = true;
+}
--- Source/Capstone_Test/MyAnimInstance.h
+++ Source/Capstone_Test/MyAnimInstance.h
@@ -13,5 +13,28 @@ UCLASS()
 class CAPSTONE_TEST_API UMyAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	UMyAnimInstance();
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	void PlayAttackMontage(UAnimMontage* GetAttackMontage);
+	void SetDeadAnim();
+
+private:
+	UPROPERTY(EditAnyWhere, BlueprintReadOnly, Category = Pawn, Meta = (AllowPrivateAccess = true))
+		float CurrentPawnSpeed; // AnimInstance를 사용해서 C++ 스크립팅 한 것을 블루프린트에서 사용이 가능하다.
+
+	UPROPERTY(EditAnyWhere, BlueprintReadOnly, Category = Pawn, Meta = (AllowPrivateAccess = true))
+		bool IsInAir;
+
+	UPROPERTY(EditAnyWhere, BlueprintReadOnly, Category = Pawn, Meta = (AllowPrivateAccess = true))
+		bool IsDead;
+
+	//UFUNCTION()
+		//void AnimNotify_SaveAttack();
+
+public:
+	UPROPERTY(EditAnyWhere, BlueprintReadOnly, Category = Pawn, Meta = (AllowPrivateAccess = true))
+		bool IsAttacking;
 };
