292544982c6683444d71df84c429439193a92d40
k1mbeaver <83329956+k1mbeaver@users.noreply.github.com>
2023-06-03 22:05:09 +0900

    Add files via upload
    AI 보스 몬스터 클래스의 코드를 추가 수정하고 디자인을 설정

--- Source/Capstone_Test/MyBossMonster.cpp
+++ Source/Capstone_Test/MyBossMonster.cpp
@@ -2,12 +2,26 @@
 
 
 #include "MyBossMonster.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 // Sets default values
 AMyBossMonster::AMyBossMonster()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	// Configure character movement
+	//GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
+	//GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
+
+	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
+	// instead of recompiling to adjust them
+	GetCharacterMovement()->JumpZVelocity = 700.f;
+	GetCharacterMovement()->AirControl = 0.35f;
+	GetCharacterMovement()->MaxWalkSpeed = 200.0f;
+	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
 
 }
 
@@ -15,7 +29,7 @@ AMyBossMonster::AMyBossMonster()
 void AMyBossMonster::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -31,4 +45,3 @@ void AMyBossMonster::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 }
-
