127d47cadab6aaeed778a1504610d31d5e5245d9
k1mbeaver <83329956+k1mbeaver@users.noreply.github.com>
2023-06-18 22:14:50 +0900

    Add files via upload
    오브젝트 컨트롤러 추가

---
+++ Source/Capstone_Test/ObjectController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "ObjectController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAPSTONE_TEST_API AObjectController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
