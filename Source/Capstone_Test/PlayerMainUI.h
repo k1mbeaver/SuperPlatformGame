b185422d3e07b2cbd44d0c38fe7625a79520b5ec
k1mbeaver <83329956+k1mbeaver@users.noreply.github.com>
2023-06-23 23:59:04 +0900

    Add files via upload
    UI를 사용하기 위한 Main, Menu UI 클래스 세팅

--- Source/Capstone_Test/PlayerMainUI.h
+++ Source/Capstone_Test/PlayerMainUI.h
@@ -13,5 +13,53 @@ UCLASS()
 class CAPSTONE_TEST_API UPlayerMainUI : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+		/*
+		UPROPERTY(Meta = (BindWidget))
+		class UPlayerGageUI* PlayerGageUI;
+
+	UPROPERTY(Meta = (BindWidget))
+		class UPlayerWeaponUI* PlayerWeaponUI;
+
+	UPROPERTY(Meta = (BindWidget))
+		class UPlayerInventoryUI* PlayerInventoryUI;
+
+	UPROPERTY(Meta = (BindWidget))
+		class UPlayerGameEndUI* PlayerEndUI;
+
+public:
+	// GageUI
+	void SetHealthPersent(float PlayerHP);
+	void SetStaminaPersent(float PlayerStamina);
+
+	// WeaponUI
+	void SetImage(UTexture2D* CurrentImage);
+	void SetImageUse();
+	void SetImageNotUse();
+	void SetImageVisible();
+	void SetImageHidden();
+	void SetProjectileText(int nProjectile);
+	void SetDefaultText(int nProjectile);
+	void SetTextVisible();
+	void SetTextHidden();
+
+	// GageAndWeaponUI
+	void SetPreviewUI();
+
+	// InventoryUI
+	void SetInventoryVisible();
+	void SetInventoryHidden();
+	void SetListCount(int nIndex, int nCount);
+	void SetListInit(int nIndex, FString strName, int nCount, UTexture2D* setImage);
+	void SetListView(int nIndex, FString strName, int nCount, UTexture2D* setImage);
+	void ListUpdate();
+	void SetWeaponImage(UTexture2D* setImage);
+
+	// EndUI
+	void SetEndVisible();
+	void SetEndHidden();
+	void SetWin();
+	void SetLose();
+	void SetEsc(bool IsEsc);
+	*/
 };
--- Source/Capstone_Test/PlayerPlayUI.h
+++ Source/Capstone_Test/PlayerPlayUI.h
@@ -13,5 +13,23 @@ UCLASS()
 class CAPSTONE_TEST_API UPlayerPlayUI : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+		/*
+private:
+	UPROPERTY(Meta = (BindWidget))
+		class UProgressBar* HealthPersent;
+
+	UPROPERTY(Meta = (BindWidget))
+		class UProgressBar* StaminaPersent;
+
+protected:
+	virtual void NativeOnInitialized() override;
+
+public:
+	// 사용할 함수들을 정리
+	void SetHealthPersent(float PlayerHP);
+	void SetStaminaPersent(float PlayerStamina);
+	void SetHidden();
+	void SetVisible();
+	*/
 };
